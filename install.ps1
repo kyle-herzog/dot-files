127ea96d576026829226876e3c050dffe89de265
Kyle Herzog <herzog.kyle@gmail.com>
2012-08-14 21:54:32 -0600

    submodule - posh-vm-management
     - added the posh-vm-management module
     - installer updated for posh-vm-management module

--- install.ps1
+++ install.ps1
@@ -1,21 +1,3 @@
-$fileName =
-@{
-  powerShellProfile = "powershell\Microsoft.PowerShell_profile.ps1"
-  originalPowerShellProfile = "powershell\Microsoft.PowerShell_profile.orig.ps1"
-  vimrc = "vim/.vimrc"
-  vimfiles = "vim/vimfiles"
-  gitconfig = "git/.gitconfig"
-  console2 = "http://sourceforge.net/projects/console/files/latest/download"
-  console = "console.xml"
-  pathogen = "vim/vimfiles/bundle/pathogen/autoload"
-  poshgit = "powershell\posh-git"
-}
-
-$directories =
-@{
-  console = 'E:\Development\Tools\Console'
-}
-
 function Get-CurrentScriptPath
 {
   Split-Path $myInvocation.ScriptName
@@ -32,22 +14,40 @@ function Get-DotFilePath
 
 function Install-DotFiles
 {
+  $installer_data =
+  @{
+    powerShellProfile = "powershell\Microsoft.PowerShell_profile.ps1"
+    originalPowerShellProfile = "powershell\Microsoft.PowerShell_profile.orig.ps1"
+    vimrc = "vim/.vimrc"
+    vimfiles = "vim/vimfiles"
+    gitconfig = "git/.gitconfig"
+    console2 = "http://sourceforge.net/projects/console/files/latest/download"
+    console = "console.xml"
+    pathogen = "vim/vimfiles/bundle/pathogen/autoload"
+    poshgit = "powershell\posh-git"
+    poshVmManagement = "powershell\posh-vm-management\install.ps1"
+    consolePath = 'E:\Development\Tools\Console'
+  }
+
   Write-Host "Initializing SubModules..." -NoNewLine
   & git submodule update --init --recursive | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Installing Posh-Git..." -NoNewLine
-  $dotFile = Get-DotFilePath $fileName.poshgit
-  $psProfile = Get-DotFilePath $fileName.originalPowerShellProfile
+  $dotFile = Get-DotFilePath $installer_data.poshgit
+  $psProfile = Get-DotFilePath $installer_data.originalPowerShellProfile
   . $psProfile
   $userpsmodules = Join-Path $HOME "Documents\WindowsPowerShell\Modules"
   if(!(Test-Path $userpsmodules))
   {
     New-Item $userpsmodules -Type Directory | Out-Null
   }
-  $installPath = Join-Path $userpsmodules (Split-Path $fileName.poshgit -leaf)
+  $installPath = Join-Path $userpsmodules (Split-Path $installer_data.poshgit -leaf)
   New-Junction $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
+
+  $dotFile = Get-DotFilePath $installer_data.poshVmManagement
+  . $dotFile
 
   Write-Host "Installing PowerShell Profile..."
   $workspacepath = "C:\dev\workspaces"
@@ -57,52 +57,68 @@ function Install-DotFiles
     $workspacepath = $promptValue
   }
   Write-Host ($workspacepath)
-  $original_file = Get-DotFilePath $fileName.originalPowerShellProfile
-  $dotFile = Get-DotFilePath $fileName.powerShellProfile
+  Write-Host ($installer_data.originalPowerShellProfile)
+
+  $original_file = Get-DotFilePath $installer_data.originalPowerShellProfile
+
+  Write-Host "Original File - $original_file"
+
+  $dotFile = Get-DotFilePath $installer_data.powerShellProfile
+
+  Write-Host "Dot File - $dotFile"
+
   if(Test-Path $dotFile)
   {
     Remove-Item $dotFile -Force
   }
+
+  Write-Host "passed dotfile check"
+
   (Get-Content $original_file) | Foreach-Object {
       $_ -replace '<workspacepath>', "$workspacepath" `
       } | Set-Content $dotFile
-  . $dotFile
+
+  Write-Host "passed dotfile customization"
+
   $userpshome = Join-Path $HOME "Documents\WindowsPowerShell"
+
+  Write-Host "UserPSHome $userpshome"
+
   if(!(Test-Path $userpshome))
   {
     New-Item $userpshome -Type Directory | Out-Null
   }
-  $installPath = Join-Path $userpshome (Split-Path $fileName.powerShellProfile -leaf)
+  $installPath = Join-Path $userpshome (Split-Path $installer_data.powerShellProfile -leaf)
   New-Symlink $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Installing vimrc..." -NoNewLine
-  $dotFile = Get-DotFilePath $fileName.vimrc
-  $installPath = Join-Path $HOME (Split-Path $fileName.vimrc -leaf)
+  $dotFile = Get-DotFilePath $installer_data.vimrc
+  $installPath = Join-Path $HOME (Split-Path $installer_data.vimrc -leaf)
   New-Symlink $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Installing vimfiles..." -NoNewLine
-  $dotFile = Get-DotFilePath $fileName.vimfiles
-  $installPath = Join-Path $HOME (Split-Path $fileName.vimfiles -leaf)
+  $dotFile = Get-DotFilePath $installer_data.vimfiles
+  $installPath = Join-Path $HOME (Split-Path $installer_data.vimfiles -leaf)
   New-Junction $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Installing pathogen autorun..." -NoNewLine
-  $dotFile = Get-DotFilepath $fileName.pathogen
+  $dotFile = Get-DotFilepath $installer_data.pathogen
   $installPath = Join-Path $installPath "autoload"
   New-Junction $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Installing gitconfig..." -NoNewLine
-  $dotFile = Get-DotFilePath $fileName.gitconfig
-  $installPath = Join-Path $HOME (Split-Path $fileName.gitconfig -leaf)
+  $dotFile = Get-DotFilePath $installer_data.gitconfig
+  $installPath = Join-Path $HOME (Split-Path $installer_data.gitconfig -leaf)
   New-Symlink $installPath $dotFile -Force | Out-Null
   Write-Host "done" -ForegroundColor DarkGreen
 
   Write-Host "Downloading console2..." -NoNewLine
 
-  Write-Host "done" -ForgroundColor DarkGreen
+  Write-Host "done" -ForegroundColor DarkGreen
 
 }
 
